df3f022cc4b711af16442e69abfef92f9ab5b15f
LostColors <417487546@qq.com>
2021-04-21 15:01:47 -0400

    Update SignIn.php

--- SignIn.php
+++ SignIn.php
@@ -20,10 +20,16 @@ foreach ($data as $user) {
         }
     }
 }
+//test code , can delete when finished
+// $validEmail=true;
+// $validPassword=true;
+// $clientName =$email;
+// $userStatus = 1;
 
 if($validEmail && $validPassword) {
     echo "<script type='text/javascript'>alert('Welcome $clientName')</script>";
-
+    //login success  write the cookies
+    writeUserCookies($clientName,$userStatus);
     if($userStatus == 1) {
         header("Refresh:1; url=BackStorePage.html");
     }
@@ -41,4 +47,11 @@ else {
         echo "<script type='text/javascript'>alert('Invalid Email.')</script>";
         header("Refresh:1; url=ShopFreshLogin.php");
     }
+}
+
+//set the user Cookies
+function  writeUserCookies($clientName,$userStatus){
+    setcookie('clientName',$clientName);
+    setcookie('userStatus',$userStatus);
+    setcookie('isLogin',true);
 }
